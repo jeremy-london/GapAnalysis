083cf242f072c613c920ae87c545dfad033c3a62
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2025-03-09 12:30:11 -0600

    add endpoint for starting bait chat

--- ana.py
+++ ana.py
@@ -1,14 +1,14 @@
 import logging
 from db import Database
-from fastapi import APIRouter, Request
-from models import Bait
+from fastapi import APIRouter, Request, StreamingResponse
+from models import Bait, ChatHistory, Sender
 from phish_interface import (
     complete_email_html,
     create_bait,
     request_linkedin_html,
     target_homepage_dict,
 )
-
+from interview_interface import begin_interview
 logger = logging.getLogger('router')
 
 router = APIRouter(
@@ -43,3 +43,16 @@ async def get_email_html(request: Request, target_name: str):
 @router.get("/welcome")
 async def welcome(request: Request):
     return "Welcome"
+
+@router.post("/start-chat/{bait_id}")
+async def start_chat(request: Request, bait_id: int) -> str:
+    db: Database = request.app.state.db
+    with db.get_session() as session:
+        db_bait:Bait = session.query(Bait).where(Bait.id==bait_id).one()
+        first_chat: ChatHistory(message=db_bait.content, sender=Sender.HUMAN, bait_id=db_bait.id)
+        first_ai_response = begin_interview(db_bait.content)
+        first_ai_chat: ChatHistory(message=first_ai_response,sender=Sender.AI,bait_id=db_bait.id)
+        session.add(first_chat)
+        session.add(first_ai_chat)
+        session.commit()
+        return first_ai_response
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from typing import Optional
-
-from sqlmodel import SQLModel, Field
-
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Column, DateTime
+from enum import Enum
 
 class BaitBase(SQLModel):
     name: str = Field(nullable=False)
@@ -11,3 +11,31 @@ class Bait(BaitBase, table=True):
     __tablename__ = "bait"
     __table_args__ = {"extend_existing": True}
     id: Optional[int] = Field(default=None, primary_key=True)
+
+class Sender(str, Enum):
+    HUMAN = "human"
+    AI = "ai"
+
+class ChatHistoryBase(SQLModel):
+    message: str = Field(nullable=False)
+    sender: str = Field(nullable=False)
+    bait_id: int = Field(foreign_key="bait.id", index=True)
+    created_at: datetime = Field(default_factory=datetime.utcnow, sa_column=Column(DateTime(timezone=True), nullable=False))
+
+    def get_message_dict(self):
+        sender_for_message = self.sender
+        return {
+            "type": sender_for_message,
+            "data": {
+                        "content": self.message or ""
+                    },
+        }
+
+class ChatHistory(ChatHistoryBase, table=True):
+    __tablename__ = "chat_history"
+    __table_args__ = {'extend_existing': True}
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    @classmethod
+    def from_base(cls, chat_history: ChatHistoryBase):
+        return cls(**chat_history.dict())

6f643b95005ba17ebced0315d1bcb68529d49df9
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2025-03-09 18:04:26 -0600

    reformat prompt

--- interview_interface.py
+++ interview_interface.py
@@ -5,7 +5,22 @@ from models import ChatHistory
 from typing import List
 client = OpenAI()
 
-SYSTEM_PROMPT = "You are interviewing a college that clicked on a bait email to check their vigilance against phishing campaigns. Interview the user to find information useful to the cybersecurity team for uncovering gaps in their training. The user will begin the interview with the html for the email they clicked on. You should then take the lead in the interview by asking them questions.\nKey questions: \nWas this email marked as EXTERNAL?\nDid you forward this to anyone?\nWhat did you think of the sender's email address before clicking on the email? (multiple choice a. didn't notice b. Thought is was safe c. Thought it was suspicious )\nDid you know you were clicking on a link? (yes/no)\nWhat did you think of the link's web address? (Similar multiple choice)\nDid you want to go to that address?\n\nAsk the questions one at a time, and add follow up questions based on their answers to better understand gaps in the company's anti-phising practice.  For example if they noticed that the email was external you should then find out what company they thought the email was from. If they forwarded the email you should find out who they forwarded it to and when. More broadly, your goal is to find out the who,what,where,when,how for broader context around these key questions. However the user will not answer well if you ask them multiple questions at one time.\n"
+SYSTEM_PROMPT = '''You are interviewing a college that clicked on a bait email to check their vigilance against phishing campaigns. 
+Interview the user to find information useful to the cybersecurity team for uncovering gaps in their training. 
+The user will begin the interview with the html for the email they clicked on. You should then take the lead in the interview by asking them questions.
+
+Key questions:
+Was this email marked as EXTERNAL?
+Did you forward this to anyone?
+What did you think of the sender's email address before clicking on the email? (multiple choice a. didn't notice b. Thought is was safe c. Thought it was suspicious )
+Did you know you were clicking on a link? (yes/no)
+What did you think of the link's web address? (Similar multiple choice)
+Did you want to go to that address?
+
+Ask the questions one at a time, and add follow up questions based on their answers to better understand gaps in the company's anti-phising practice.
+For example if they noticed that the email was external you should then find out what company they thought the email was from. If it was not external found out if someone at the comapny forwarded it to them. If they forwarded the email you should find out who they forwarded it to and when. 
+More broadly, your goal is to find out the who,what,where,when,how for broader context around these key questions.
+However the user will not answer well if you ask them multiple questions at one time.\n'''
 
 def begin_interview(bait_context:str)->str:
     # this can be AI but the first message really does not need to be so I will use text
